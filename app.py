e64cdd73524bae9454bfac412b99a75cc187228c
Daniel Hettle <dan@Daniels-MBP.lan>
2025-07-26 15:24:04 -0700

    Preparing to sync with remote

--- app.py
+++ app.py
@@ -3,37 +3,18 @@ import torch
 import torch.nn as nn
 import numpy as np
 import os
-import gdown
-import requests
 from PIL import Image
 from torchvision.models import efficientnet_b4, EfficientNet_B4_Weights
 from torchvision import transforms
 
-# Function to download model if not present
-def download_model_from_gdrive(file_id, dest_path):
-    if not os.path.exists(dest_path):
-        st.write("Downloading model from Google Drive...")
-        url = f"https://drive.google.com/uc?id={file_id}"
-        response = requests.get(url)
-        with open(dest_path, "wb") as f:
-            f.write(response.content)
-        st.write("Model download complete.")
-
-# Download your model from Google Drive
-download_model_from_gdrive("1HNlLg7q1oH-Od5LWsN9Cuo5VXFcNBRw6", "model_fold_1.pth")
-
 # Load model
-
 @st.cache_resource
 def load_model():
     model = efficientnet_b4(weights=EfficientNet_B4_Weights.DEFAULT)
     model.classifier[0] = nn.Dropout(p=0.3)
-    model.classifier[1] = nn.Linear(model.classifier[1].in_features, 2)  # 2 output classes
+    model.classifier[1] = nn.Linear(model.classifier[1].in_features, 2)
 
-    # Load the model weights from Reid's checkpoint
-    dest_path = "model_fold_1.pth"
-    download_model_from_gdrive("1HNlLg7q1oH-Od5LWsN9Cuo5VXFcNBRw6", dest_path)   
-    checkpoint = torch.load(dest_path, map_location=torch.device("cpu"))
+    checkpoint = torch.load("models/model_fold_1.pth", map_location=torch.device('cpu'))
     model.load_state_dict(checkpoint["model_state_dict"])
     model.eval()
     return model
